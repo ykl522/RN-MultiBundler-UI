9558af1d08911811d531b4912ad96e84d4c896e6
袁康乐 <yuankangle@yunexpress.cn>
2022-11-03 14:36:38 +0800

    优化打包和多语言

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,7 @@
  * @Author: 袁康乐 yuankangle@yunexpress.cn
  * @Date: 2021-07-02 14:48:11
  * @LastEditors: 袁康乐 yuankangle@yunexpress.cn
- * @LastEditTime: 2022-10-28 17:57:30
+ * @LastEditTime: 2022-11-02 15:15:32
  * @FilePath: \RN-MultiBundler-UI\src\app.jsx
  * @Description: 首页
  */
@@ -19,6 +19,9 @@ class App extends React.Component {
 
 	constructor(props) {
 		super(props);
+		this.state = {
+			activeKey: 'item-1'
+		}
 	}
 
 	componentDidMount() {
@@ -27,13 +30,20 @@ class App extends React.Component {
 
 	render() {
 		const items = [
-			{ label: '打包', key: 'item-1', children: <PackageView goUpload={() => { }} /> },
+			{ label: '打包', key: 'item-1', children: <PackageView goUpload={() => { this.setState({ activeKey: 'item-4' }) }} /> },
 			{ label: '多语言', key: 'item-2', children: <LanguageView projDir={workSpace} /> },
 			{ label: '二维码', key: 'item-3', children: <QRCodeView /> },
 			{ label: '接口', key: 'item-4', children: <ApiView /> },
 		];
 		return (
-			<Tabs tabBarStyle={{ paddingLeft: 30, }} items={items} />
+			<Tabs
+				tabBarStyle={{ paddingLeft: 30, }}
+				items={items}
+				activeKey={this.state.activeKey}
+				onChange={(activeKey) => {
+					this.setState({ activeKey })
+				}}
+			/>
 		);
 	}
 }
