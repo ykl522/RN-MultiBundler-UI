82ff671af79d4ace5cda16c9d61d5c9ebefbc314
康乐 <yuankangle@yunexpress.cn>
2023-01-11 18:08:09 +0800

    新增apk解析工具

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 /*
  * @Author: 袁康乐 yuankangle@yunexpress.cn
  * @Date: 2021-07-02 14:48:11
- * @LastEditors: 袁康乐 yuankangle@yunexpress.cn
- * @LastEditTime: 2022-11-10 11:30:13
+ * @LastEditors: 康乐 yuankangle@yunexpress.cn
+ * @LastEditTime: 2023-01-10 15:59:16
  * @FilePath: \RN-MultiBundler-UI\src\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-const { app, BrowserWindow, Menu, ipcMain } = require('electron');
+const { app, BrowserWindow, Menu, ipcMain, shell } = require('electron');
 const { REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer');
 const installExtension = require('electron-devtools-installer').default;
 const { enableLiveReload } = require('electron-compile');
@@ -98,12 +98,23 @@ const createWindow = async () => {
       label: '帮助',
       submenu: [
         {
-          label: '访问官网', click: async () => {
+          label: '访问Electron官网', click: async () => {
             const { shell } = require('electron')
             await shell.openExternal('https://www.electronjs.org/docs/latest/api/app')
           }
         },
-        { label: '关于' }
+        {
+          label: '访问工具Github地址', click: async () => {
+            const { shell } = require('electron')
+            await shell.openExternal('https://github.com/ykl522/RN-MultiBundler-UI')
+          }
+        },
+        {
+          label: '关于', click: async () => {
+            const { dialog } = require('electron')
+            await dialog.showMessageBox({ title: "关于", message: '云途APP开发工具\n版本号：1.0.1', type: 'info' })
+          }
+        }
       ]
     }
   ];
--- src/page/PackageView.js
+++ src/page/PackageView.js
@@ -3,7 +3,7 @@
  * @Author: 袁康乐 yuankangle@yunexpress.cn
  * @Date: 2022-10-21 16:37:25
  * @LastEditors: 康乐 yuankangle@yunexpress.cn
- * @LastEditTime: 2023-01-04 10:13:47
+ * @LastEditTime: 2023-01-11 13:50:19
  * @FilePath: \RN-MultiBundler-UI\src\page\PackageView.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
