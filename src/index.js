6b128c2fd166f74e79934138ffd9e4c41a409c49
康乐 <yuankangle@yunexpress.cn>
2023-03-21 16:18:33 +0800

    main.css不存在，导致热加载失败，去掉引入

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * @Author: 袁康乐 yuankangle@yunexpress.cn
  * @Date: 2021-07-02 14:48:11
  * @LastEditors: 康乐 yuankangle@yunexpress.cn
- * @LastEditTime: 2023-03-21 11:23:02
+ * @LastEditTime: 2023-03-21 16:16:11
  * @FilePath: \RN-MultiBundler-UI\src\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -24,7 +24,7 @@ const isDevMode = process.execPath.match(/[\\/]electron/);
 
 if (isDevMode) enableLiveReload({ strategy: 'react-hmr' });
 
-const createLoadingWindow = async () => {
+const createLoadingWindow = () => {
   loadingWindow = new BrowserWindow({
     width: 600,
     height: 300,
