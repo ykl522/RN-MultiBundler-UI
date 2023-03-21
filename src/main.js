c250f56a9ca94321a024e1af61b76087fde9ac6f
康乐 <yuankangle@yunexpress.cn>
2023-03-21 16:18:33 +0800

    main.css不存在，导致热加载失败，去掉引入

--- src/main.js
+++ src/main.js
@@ -2,9 +2,8 @@
  * @Author: 袁康乐 yuankangle@yunexpress.cn
  * @Date: 2022-09-23 16:21:57
  * @LastEditors: 康乐 yuankangle@yunexpress.cn
- * @LastEditTime: 2023-02-27 17:27:04
+ * @LastEditTime: 2023-03-21 16:03:51
  * @FilePath: \RN-MultiBundler-UI\src\main.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 const React = require('react');
 const ReactDOM = require('react-dom');
