f9501a7c26c13616089be011e9e605282e1c4eeb
康乐 <yuankangle@yunexpress.cn>
2023-03-21 15:28:09 +0800

    日志工具优化，增加清除功能 项目管理工具增加打包功能 入口增加loading界面

--- src/page/LogView.js
+++ src/page/LogView.js
@@ -2,7 +2,7 @@
  * @Author: 康乐 yuankangle@yunexpress.cn
  * @Date: 2023-03-17 15:44:57
  * @LastEditors: 康乐 yuankangle@yunexpress.cn
- * @LastEditTime: 2023-03-17 18:45:06
+ * @LastEditTime: 2023-03-21 10:32:03
  * @FilePath: \RN-MultiBundler-UI\src\page\Model.js
  * @Description: 日志
  */
@@ -14,7 +14,10 @@ import { DownOutlined } from '@ant-design/icons';
 export default (props) => {
     const filtersRef = useRef()
     const [logContent, setLogContent] = useState("")
-    const devices = useRef([])
+    const logContentRef = useRef({ text: '', isExe: false })
+    const logTextAreaRef = useRef('')
+    const [devices, setDevices] = useState([])
+    const devicesRef = useRef([])
     const [selectDevice, setSelectDevicce] = useState('')
     const [api, contextHolder] = notification.useNotification();
     const openNotification = (des, placement = 'bottomRight') => {
@@ -25,35 +28,46 @@ export default (props) => {
         });
     };
 
-    console.log("tabChangeKey===>" + props.tabChangeKey)
-    if (props.tabChangeKey == 'item-9') {
-        WinExec.cmd(`nox_adb devices`, null, (data) => {
-            let list = `${data}`.split('\n')
-            devices.current = []
-            for (let device of list) {
-                if (device.trim() && !device.includes('List of devices attached')) {
-                    let deviceObj = { key: devices.current.length + '', label: device.replace('device', '').trim() }
-                    devices.current.push(deviceObj)
-                }
-            }
+    useEffect(() => {
+        logContentRef.current.isExe = false
+    }, [selectDevice])
 
-        })
+    const getDevices = () => {
+        if (props.tabChangeKey == 'item-9') {
+            console.log('-----------------获取adb设备----------------')
+            WinExec.cmd(`nox_adb devices`, null, (data) => {
+                let list = `${data}`.split('\n')
+                devicesRef.current = []
+                for (let device of list) {
+                    if (device.trim() && !device.includes('List of devices attached')) {
+                        let deviceObj = { key: devicesRef.current.length + '', label: device.replace('device', '').trim() }
+                        devicesRef.current.push(deviceObj)
+                    }
+                }
+                console.log('-----------------刷新adb设备----------------' + devicesRef.current.length)
+                setDevices(devicesRef.current)
+            })
+        }
     }
 
+    // 日志改变时 输入框一直保持滚动到最底部
     useEffect(() => {
-    }, [])
+        if (logTextAreaRef.current && logTextAreaRef.current) {
+            logTextAreaRef.current.resizableTextArea.textArea.scrollTop = logTextAreaRef.current.resizableTextArea.textArea.scrollHeight
+        }
+    }, [logContent])
 
     const devicesMenu = (
         <Menu
             selectable
             onClick={(e) => {
                 console.log(e.key)
-                console.log(devices.current)
-                if (devices.current[e.key].label) {
-                    setSelectDevicce(devices.current[e.key])
+                console.log(devices)
+                if (devices[e.key].label) {
+                    setSelectDevicce(devices[e.key])
                 }
             }}
-            items={devices.current}
+            items={devices}
         />
     );
 
@@ -62,9 +76,21 @@ export default (props) => {
             {contextHolder}
             <Space >
                 <Input placeholder={'请输入要过滤内容'} onChange={(e) => {
+                    if (logContentRef.current)
+                        logContentRef.current.isExe = false
                     filtersRef.current = e.target.value
                 }} />
-                <Dropdown overlay={devicesMenu} trigger={['click']} selectable>
+                <Dropdown
+                    overlay={devicesMenu}
+                    trigger={['click']}
+                    selectable
+                    onOpenChange={(flag) => {
+                        console.log('--------onOpenChange---------' + flag)
+                        if (flag) {
+                            getDevices()
+                        }
+                    }}
+                >
                     <Space>
                         {selectDevice && selectDevice.label || '请选择设备'}
                         <DownOutlined />
@@ -74,10 +100,15 @@ export default (props) => {
                     if (!filtersRef || !filtersRef.current) {
                         openNotification('请输入要过滤内容!')
                     } else {
-                        WinExec.cmd(`nox_adb -s ${selectDevice.label} logcat |find /I "${filtersRef.current}"`, null, (data) => {
-                            // logContent += data
-                            setLogContent(data)
-                        })
+                        if (!logContentRef.current.isExe) {
+                            WinExec.cmd(`nox_adb -s ${selectDevice.label} logcat |find /I "${filtersRef.current}"`, null, (data) => {
+                                logContentRef.current.isExe = true
+                                logContentRef.current.text = logContentRef.current.text + data
+                                setLogContent(logContentRef.current.text)
+                            })
+                        } else {
+                            openNotification('正在获取中!')
+                        }
                     }
                 }}>获取日志</Button>
                 <Button onClick={() => {
@@ -85,13 +116,22 @@ export default (props) => {
                         openNotification('请输入要过滤内容!')
                     } else {
                         WinExec.cmd(`start cmd /k "CHCP 65001 && nox_adb -s ${selectDevice.label} logcat |find /I \"${filtersRef.current}\""`, null, (data) => {
-                            // logContent += data
-                            setLogContent(data)
+                            logContentRef.current.text = logContentRef.current.text + data
+                            setLogContent(logContentRef.current.text)
                         })
                     }
                 }}>CMD获取日志</Button>
+                <Button onClick={() => {
+                    logContentRef.current.text = ""
+                    setLogContent("")
+                }}>清除日志</Button>
             </Space>
-            <Input.TextArea rows={20} style={{ marginTop: 10 }} value={logContent} />
+            <Input.TextArea
+                ref={logTextAreaRef}
+                rows={20}
+                style={{ marginTop: 10 }}
+                value={logContent}
+            />
         </div>
     )
 }
