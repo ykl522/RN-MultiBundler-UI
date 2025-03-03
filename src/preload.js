f3dda9b7cb8ceb9e6915986cef6b51b5e62214c9
康乐 <yuankangle@gmail.com>
2025-03-03 16:53:16 +0800

    注释优化

--- src/preload.js
+++ src/preload.js
@@ -1,7 +1,7 @@
 /*
- * @Author: 袁康乐 yuankangle@gmail.com
+ * @Author: 康乐 yuankangle@gmail.com
  * @Date: 2022-11-03 14:37:26
- * @LastEditors: 袁康乐 yuankangle@gmail.com
+ * @LastEditors: 康乐 yuankangle@gmail.com
  * @LastEditTime: 2022-11-10 11:26:48
  * @FilePath: \RN-MultiBundler-UI\src\preload.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
